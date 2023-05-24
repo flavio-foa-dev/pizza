c80d77f8f5103846e18221f79fed74de51cc4fdd
Flavio Andrade <foa12381@gmail.com>
2023-05-24 10:04:40 -0300

    order css logica

--- src/context/Search.tsx
+++ src/context/Search.tsx
@@ -1,8 +1,10 @@
 import {ReactNode, SetStateAction, createContext, useState} from "react"
 
 type Contexte = {
-  search:string;
-  setSearch?: React.Dispatch<SetStateAction<string>>
+  search?:string;
+  setSearch?: React.Dispatch<SetStateAction<string>>;
+  sort?:string
+  setSort?: React.Dispatch<SetStateAction<string>>
 }
 
 export const ContextSearch = createContext<Contexte | undefined>(undefined)
@@ -13,8 +15,9 @@ type Children = { children: ReactNode}
 export function ProviderSearch({children}:Children) {
   const [search, setSearch] = useState('')
   const [filter, setFilter] = useState(null)
+  const [sort, setSort] = useState('')
 
-  const value={search, setSearch, filter, setFilter}
+  const value={search, setSearch, filter, setFilter, sort, setSort}
   return <ContextSearch.Provider value={value}>
     {children}
   </ContextSearch.Provider>
