18c53494cbb39f375450d1c2dac4a9fdf7c21b8e
Flavio Andrade <foa12381@gmail.com>
2023-05-26 12:45:23 -0300

    menu navigation

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -1,10 +1,14 @@
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import Home from '../pages/Home/Home';
 import Products from '../pages/Products/Products';
+import Navagation from '../Components/Navegation/Navagation';
+import Header from '../Components/Header/Header';
 
 export default function AppRouter() {
   return(
     <BrowserRouter>
+      <Navagation/>
+      <Header />
       <Routes>
         <Route path="/" element={<Home/>}/>
         <Route path='/products' element={<Products/>}/>
