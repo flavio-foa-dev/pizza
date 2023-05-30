cb8b9ffb93be2dfb48d393bad304abc3b7db3a0b
Flavio Andrade <foa12381@gmail.com>
2023-05-30 19:30:42 -0300

    refactoring header

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -1,10 +1,8 @@
 import styles from './footer.module.css';
-import  Logo  from '../../assets/logo.svg';
 
 export default function Footer() {
   return (
     <footer className={styles.footer}>
-      <img src={Logo} alt="logo" />
     </footer>
   );
 }
